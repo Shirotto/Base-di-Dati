787dd2abd8e5429b84121504606b7c84dd7d4e27
Mario <130995608+Microchip-Rezet@users.noreply.github.com>
2024-05-29 20:13:49 +0200

    Update Approvazione Prod. Candidato.sql

--- Approvazione Prod. Candidato.sql
+++ Approvazione Prod. Candidato.sql
@@ -1,19 +1,19 @@
 DELIMITER //
 
 CREATE PROCEDURE ApprovaProdottoCandidato(
-    IN p_ID_richiesta_acquisto INT,
-    IN p_ID_prodotto_candidato INT
+    IN AP_ID_richiesta_acquisto INT,
+    IN AP_ID_prodotto_candidato INT
 )
 BEGIN
-    DECLARE v_count INT;
+    DECLARE ver_count INT;
 
     -- Verifica se la combinazione di ID richiesta e ID prodotto candidato esiste
-    SELECT COUNT(*) INTO v_count
+    SELECT COUNT(*) INTO ver_count
     FROM prodotto_candidato
-    WHERE ID = p_ID_prodotto_candidato AND ID_richiesta_acquisto = p_ID_richiesta_acquisto;
+    WHERE ID = AP_ID_prodotto_candidato AND ID_richiesta_acquisto = AP_ID_richiesta_acquisto;
 
     -- se non esiste una combinazione allora genera un'eccezine personalizzata
-    IF v_count = 0 THEN
+    IF ver_count = 0 THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'La combinazione di richiesta d\'acquisto e prodotto candidato non esiste.;
     ELSE
@@ -21,7 +21,7 @@ BEGIN
         -- Aggiorna lo stato di approvazione del prodotto candidato
         UPDATE prodotto_candidato
         SET approvazione = TRUE
-        WHERE ID = p_ID_prodotto_candidato AND ID_richiesta_acquisto = p_ID_richiesta_acquisto;
+        WHERE ID = AP_ID_prodotto_candidato AND ID_richiesta_acquisto = AP_ID_richiesta_acquisto;
     END IF;
 END //
 
