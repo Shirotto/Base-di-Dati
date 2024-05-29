6c522dedc61148a49d29451ae5085be04198f64c
Mario <130995608+Microchip-Rezet@users.noreply.github.com>
2024-05-29 20:11:34 +0200

    Update and rename Approvazione Prod. Candidato to Approvazione Prod. Candidato.sql

--- Approvazione Prod. Candidato.sql
+++ Approvazione Prod. Candidato.sql
@@ -12,15 +12,18 @@ BEGIN
     FROM prodotto_candidato
     WHERE ID = p_ID_prodotto_candidato AND ID_richiesta_acquisto = p_ID_richiesta_acquisto;
 
+    -- se non esiste una combinazione allora genera un'eccezine personalizzata
     IF v_count = 0 THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'La combinazione di richiesta d\'acquisto e prodotto candidato non esiste.';
+        SET MESSAGE_TEXT = 'La combinazione di richiesta d\'acquisto e prodotto candidato non esiste.;
     ELSE
+        
         -- Aggiorna lo stato di approvazione del prodotto candidato
         UPDATE prodotto_candidato
         SET approvazione = TRUE
         WHERE ID = p_ID_prodotto_candidato AND ID_richiesta_acquisto = p_ID_richiesta_acquisto;
     END IF;
 END //
+
 
 DELIMITER ;
