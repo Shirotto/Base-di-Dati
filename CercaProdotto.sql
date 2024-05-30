1a87b04f5cfbca2ef09d2818e4ae2464856f65b4
Shirotto <127602582+Shirotto@users.noreply.github.com>
2024-05-30 17:20:51 +0200

    Update CercaProdotto.sql

--- CercaProdotto.sql
+++ CercaProdotto.sql
@@ -1,20 +1,21 @@
--- Procedura usata dal tecnico per inserire il prodotto trovato nel sistema
+-- Procedura usata dall tecnico per inserire il prodotto trovato nel sistema
 DELIMITER //
 CREATE PROCEDURE CercaProdotto(
 IN nome_prodotto VARCHAR(100),
 IN nome_produttore VARCHAR(100), 
 IN codice_prodotto INT, 
 IN url_prodotto VARCHAR(100),
-IN prezzo INT,
-IN approvazione BOOLEAN)
+IN note VARCHAR(100),
+IN prezzo INT
+)
 BEGIN 
 
-INSERT INTO prodotto_candidato(nome_prodotto,nome_produttore,codice_prodotto,URL_info,prezzo,approvazione) VALUES (nome_prodotto,nome_produttore,codice_prodotto,url_prodotto,prezzo,approvazione);
+INSERT INTO prodotto_candidato(nome_prodotto,nome_produttore,codice_prodotto,URL_info,note,prezzo,approvazione) VALUES (nome_prodotto,nome_produttore,codice_prodotto,url_prodotto,note,prezzo,approvazione);
 
 END //
 DELIMITER ;
 
-CALL CercaProdotto('asis notebook','asis company',1,'www.asis',1080,FALSE);
-CALL CercaProdotto('applo notebook','applo company',2,'www.indirizzo',2000,FALSE);
-CALL CercaProdotto('pera phone','pera inc',3,'www.pera',750,FALSE);
+CALL CercaProdotto('asis notebook','asis company',1,'www.asis','tastiera colorata',1080);
+CALL CercaProdotto('applo notebook','applo company',2,'www.indirizzo','telecamera',2000);
+CALL CercaProdotto('pera phone','pera inc',3,'www.pera','dedica',750);
 SELECT * FROM prodotto_candidato;
