87422e1d2557ecb8dce0e4cba3523755fb2eff0d
Shirotto <127602582+Shirotto@users.noreply.github.com>
2024-05-30 11:16:57 +0200

    Update Elimina Richiesta D'acquisto.sql

--- Elimina Richiesta D'acquisto.sql
+++ Elimina Richiesta D'acquisto.sql
@@ -3,14 +3,11 @@ CREATE PROCEDURE EliminaRichiestaAcquisto(
 IN id_richiesta INT
 )
 BEGIN 
-
-
+-- Elimina la riga relativa all'id inserito nella tabella propone
 DELETE FROM propone WHERE ID_richiesta_acquisto = id_richiesta;
-
-
+-- Elimina la riga relativa all'id inserito nella tabella seleziona
 DELETE FROM seleziona WHERE ID_richiesta_acquisto = id_richiesta;
-
-
+-- Elimina la riga relativa all'id inserito nella tabella richista d'acquisto
 DELETE FROM richiesta_acquisto WHERE ID = id_richiesta;
 
 SELECT 'Richiesta eliminata' AS risultato;
