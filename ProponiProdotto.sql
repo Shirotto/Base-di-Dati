a0f039905242306f459ae710a1247ecab4b8db43
Shirotto <127602582+Shirotto@users.noreply.github.com>
2024-05-30 17:06:58 +0200

    Update ProponiProdotto.sql

--- ProponiProdotto.sql
+++ ProponiProdotto.sql
@@ -3,17 +3,13 @@ CREATE PROCEDURE ProponiProdotto(
     IN id_richiesta INT,
     IN id_prodotto INT
 )
-BEGIN  
+BEGIN    
+ -- Aggiorna la richiesta d'acquisto
+UPDATE richiesta_acquisto SET ID_prodottoass = id_prodotto WHERE ID = id_richiesta;
 
-UPDATE richiesta_acquisto 
-SET 
-    ID_prodottoass = id_prodotto
-WHERE
-    ID = id_richiesta;
-
+-- Aggiorna lo stato delle proposte di acquisto
 INSERT INTO propone (stato, ID_richiesta_acquisto, ID_prodotto_candidato)
 VALUES ('In attesa', id_richiesta, id_prodotto);
-
     
 END //
 
