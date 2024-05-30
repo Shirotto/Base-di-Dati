6a1d5a6e4fe3dff74bc69a944e312dab0a3f72cb
Marco-Nico <132138568+Marco-Nico@users.noreply.github.com>
2024-05-30 12:22:17 +0200

    Create associa_ra.sql

---
+++ associa_ra.sql
@@ -0,0 +1,37 @@
+create view Tecnici as    
+select ID, num_richieste_associate
+from utente
+where tipo = 'tecnico';
+
+
+delimiter \\
+
+CREATE PROCEDURE assegna_ra(IN ra_ID INTEGER)
+
+
+begin
+
+DECLARE tecnico INTEGER;
+
+SELECT 
+    ID
+INTO tecnico FROM
+    Tecnici
+GROUP BY Tecnici.ID
+ORDER BY Tecnici.num_richieste_associate ASC
+LIMIT 1;
+
+
+UPDATE richiesta_acquisto 
+SET 
+    tecnico_assegnato = tecnico
+WHERE
+    richiesta_acquisto.ID = ra_ID;
+UPDATE utente 
+SET 
+    num_richieste_associate = num_richieste_associate + 1
+WHERE
+    utente.ID = tecnico;
+end \\
+
+delimiter ;
