7af90a201a705df1e6bbb0c7151a6b348a79defe
Shirotto <127602582+Shirotto@users.noreply.github.com>
2024-05-30 11:32:51 +0200

    Update Richiesta_acquisto.sql

--- Richiesta_acquisto.sql
+++ Richiesta_acquisto.sql
@@ -3,17 +3,17 @@ CREATE PROCEDURE CreareRichiesta(
  IN id_utente INT, categoria_selezionata VARCHAR(100)
  )
 BEGIN 
--- Variabile per tener conto del numero di richieste
+-- Variabili usate per tener conto del numero delle richieste di acquisto da utilizzare localmente per il funzionamento di metodi nella procedura
 DECLARE max_numero_richiesta INT;
 DECLARE id_richiesta_acquisto INT;
 
--- Trova il massimo numero di richieste e lo incrementa di 1
+-- Metodo per trovare il massimo numero delle richieste d'acquisto e lo incementa di 1
 SELECT 
-    IFNULL(MAX(numero_richiesta), 0) + 1
+    IFNULL(MAX(ID), 0) + 1
 INTO max_numero_richiesta FROM
     richiesta_acquisto;
 
--- Trova l'ID della categoria "PERSONAL COMPUTER"
+-- Metodo per trovare ID della categoria 
 SELECT 
     ID
 INTO @id_categoria FROM
@@ -21,24 +21,28 @@ INTO @id_categoria FROM
 WHERE
     nome = categoria_selezionata;
 
--- Inserisci una nuova richiesta d'acquisto
-INSERT INTO richiesta_acquisto (ID_utente, totale, `data`, numero_richiesta)
-VALUES (id_utente, 250.00, NOW(), max_numero_richiesta);
+-- Metodo per inserire la rischiera con i valori
+INSERT INTO richiesta_acquisto (ID_utente, totale, `data`)
+VALUES (id_utente, 250.00, NOW());
 
--- Trova l'ID della nuova richiesta d'acquisto
+-- Metodo per trovare ID della richista d'acquisto
 SELECT 
     ID
 INTO id_richiesta_acquisto FROM
     richiesta_acquisto
 WHERE
-    numero_richiesta = max_numero_richiesta;
+    ID = max_numero_richiesta;
 
--- Associa la categoria "PERSONAL COMPUTER" alla richiesta d'acquisto
+-- Metodo per associare l' ID della richiesta all' ID della categoria
 INSERT INTO seleziona (ID_richiesta_acquisto, ID_categoria)
 VALUES (id_richiesta_acquisto, @id_categoria);
 
  END //
         DELIMITER ;
+        
+CALL CreareRichiesta( 3 , 'PERSONAL COMPUTER');
+CALL CreareRichiesta( 1 , 'PERSONAL COMPUTER');
+select * FROM richiesta_acquisto;
         
 CALL CreareRichiesta( 3 , 'PERSONAL COMPUTER');
 CALL CreareRichiesta( 1 , 'PERSONAL COMPUTER');
