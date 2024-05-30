e113f83b99abf9b9c9f827044fbb2c02af02c809
Shirotto <127602582+Shirotto@users.noreply.github.com>
2024-05-30 11:15:09 +0200

    Update Elimina Richiesta D'acquisto.sql

--- Elimina Richiesta D'acquisto.sql
+++ Elimina Richiesta D'acquisto.sql
@@ -1,22 +1,21 @@
 DELIMITER //
--- Creo una procedura che elimina la richiesta d'acquisto data in input
 CREATE PROCEDURE EliminaRichiestaAcquisto(
-IN richiesta_id INT
+IN id_richiesta INT
 )
 BEGIN 
 
--- Rimuove le informazione della richiesta selezionata dalla tabella propone
-DELETE FROM propone WHERE ID_richiesta_acquisto = richiesta_id;
 
--- Rimuove le informazione della richiesta selezionata dalla tabella seleziona
-DELETE FROM seleziona WHERE ID_richiesta_acquisto = richiesta_id;
+DELETE FROM propone WHERE ID_richiesta_acquisto = id_richiesta;
 
--- Rimuove i prodotti candidati associati alla richiesta d'acquisto
-DELETE FROM prodotto_candidato WHERE ID_richiesta_acquisto = richiesta_id;
 
--- Rimuove la richiesta d'acquisto dalla tabella "richiesta_acquisto"
-DELETE FROM richiesta_acquisto WHERE ID = richiesta_id;
+DELETE FROM seleziona WHERE ID_richiesta_acquisto = id_richiesta;
+
+
+DELETE FROM richiesta_acquisto WHERE ID = id_richiesta;
 
 SELECT 'Richiesta eliminata' AS risultato;
-        END //
-        DELIMITER ;
+
+END //
+DELIMITER ;
+        
+CALL EliminaRichiestaAcquisto(1);
