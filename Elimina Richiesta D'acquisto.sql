dec699a3ee1d762d6803b4bfc3df1e70387e7d81
Shirotto <127602582+Shirotto@users.noreply.github.com>
2024-05-31 16:25:36 +0200

    Update Elimina Richiesta D'acquisto.sql

--- Elimina Richiesta D'acquisto.sql
+++ Elimina Richiesta D'acquisto.sql
@@ -1,13 +1,18 @@
+SET SQL_SAFE_UPDATES = 0;
+
+-- Procedura usate per eliminare la richiesta d'acquisto nella sua tabella e anche nelle tabelle in cui appare
 DELIMITER //
 CREATE PROCEDURE EliminaRichiestaAcquisto(
 IN id_richiesta INT
 )
 BEGIN 
--- Elimina la riga relativa all'id inserito nella tabella propone
+
 DELETE FROM propone WHERE ID_richiesta_acquisto = id_richiesta;
--- Elimina la riga relativa all'id inserito nella tabella seleziona
+
 DELETE FROM seleziona WHERE ID_richiesta_acquisto = id_richiesta;
--- Elimina la riga relativa all'id inserito nella tabella richista d'acquisto
+
+DELETE FROM associa WHERE ID_richiesta_acquisto = id_richiesta;
+
 DELETE FROM richiesta_acquisto WHERE ID = id_richiesta;
 
 SELECT 'Richiesta eliminata' AS risultato;
@@ -16,3 +21,12 @@ END //
 DELIMITER ;
         
 CALL EliminaRichiestaAcquisto(1);
+
+
+SELECT * FROM richiesta_acquisto;
+SELECT * FROM propone;
+SELECT * FROM seleziona;
+SELECT * FROM associa;
+
+
+SET SQL_SAFE_UPDATES = 1;
