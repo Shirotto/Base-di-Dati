8bd1cbfead923741bcc14d71d6b83f3cef539613
Shirotto <127602582+Shirotto@users.noreply.github.com>
2024-05-31 11:27:14 +0200

    Update and rename ProponiProdotto.sql to 4ProponiProdotto.sql

---
+++ 4ProponiProdotto.sql
@@ -0,0 +1,24 @@
+DELIMITER //
+CREATE PROCEDURE ProponiProdotto(
+    IN id_richiesta INT,
+    IN id_prodotto INT
+)
+BEGIN    
+ -- Aggiunge alla richiesta d'acquisto il prodotto candidato
+UPDATE richiesta_acquisto SET ID_prodottoass = id_prodotto WHERE ID = id_richiesta;
+
+-- Aggiunge alla richiesta d'acquisto il prezzo del prodotto candidato
+SELECT prezzo INTO @prezzo FROM prodotto_candidato WHERE ID = id_prodotto;
+UPDATE richiesta_acquisto SET totale = @prezzo WHERE ID = id_richiesta;
+
+-- Aggiorna lo stato delle proposte di acquisto
+INSERT INTO propone (ID_richiesta_acquisto, ID_prodotto_candidato)
+VALUES (id_richiesta, id_prodotto);
+    
+END //
+
+DELIMITER ;
+CALL ProponiProdotto( 1 , 2);
+select * FROM richiesta_acquisto;
+SELECT * FROM prodotto_candidato;
+SELECT * FROM propone;
