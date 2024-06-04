b36ed8600098ccb0babfc4360dbed77306b56233
Shirotto <127602582+Shirotto@users.noreply.github.com>
2024-06-04 15:25:56 +0200

    Update 5 richieste di un ordinante in sospeso.sql

--- 5 richieste di un ordinante in sospeso.sql
+++ 5 richieste di un ordinante in sospeso.sql
@@ -2,15 +2,12 @@ DELIMITER //
 
 CREATE PROCEDURE RANC_Ordinante (IN ID_Ordinante INTEGER UNSIGNED)
 BEGIN
-    SELECT *
-    FROM richiesta_acquisto ra
-    JOIN prodotto_candidato pc ON ra.ID_prodottoass = pc.ID
-    WHERE ra.ID_utente = ID_Ordinante
-      AND ra.stato_richiesta = 'aperta'
-      AND pc.approvazione_prodotto_candidato = 'in valutazione';
+    SELECT * FROM richiesta_acquisto WHERE richiesta_acquisto.ID_utente = ID_Ordinante
+      AND stato_richiesta = 'aperta'
+      AND approvazione_prodotto_candidato = 'in valutazione';
 END //
 
 DELIMITER ;
 
 
-CALL RANC_Ordinante(5);
+CALL RANC_Ordinante(6);
