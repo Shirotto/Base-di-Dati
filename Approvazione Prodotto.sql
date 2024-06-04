f7eb9fef0b20265e23293ad6f0c160dadcf8608d
Shirotto <127602582+Shirotto@users.noreply.github.com>
2024-06-04 15:22:28 +0200

    Update Approvazione Prodotto.sql

--- Approvazione Prodotto.sql
+++ Approvazione Prodotto.sql
@@ -1,34 +1,15 @@
 DELIMITER // 
 
 CREATE PROCEDURE ApprovaProdottoCandidato(
-   IN richiestaID INT,
-   IN prodottoID INT
+   IN richiestaID INT
 )
 BEGIN
-    DECLARE prod_ass INT;
+    
+    UPDATE richiesta_acquisto SET approvazione_prodotto_candidato = 'approvato' WHERE richiesta_acquisto.ID = richiestaID;
 
-    -- Verifica se la combinazione di ID richiesta e ID prodotto candidato esiste
-SELECT 
-    COUNT(*)
-INTO prod_ass FROM
-    propone
-WHERE
-    ID_richiesta_acquisto = richiestaID
-        AND ID_prodotto_candidato = prodottoID;
-
-   /* IF prod_ass = 0 THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'La combinazione di richiesta d\'acquisto e prodotto candidato non esiste.';
-    ELSE*/
-UPDATE prodotto_candidato 
-SET 
-    approvazione_prodotto_candidato = 'approvato'
-WHERE
-    ID = prodottoID; 
-
-END IF;
 END //
 
 DELIMITER ;
-CALL ApprovaProdottoCandidato(1,2);
+CALL ApprovaProdottoCandidato(1000);
 SELECT * FROM prodotto_candidato;
+SELECT * FROM richiesta_acquisto;
