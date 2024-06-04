9709eee373c360fa08466143ad4004ff377d3dd1
Shirotto <127602582+Shirotto@users.noreply.github.com>
2024-06-04 15:19:47 +0200

    Update 1Richiesta_acquisto.sql

--- 1Richiesta_acquisto.sql
+++ 1Richiesta_acquisto.sql
@@ -3,24 +3,22 @@ CREATE PROCEDURE CreareRichiesta(
  IN id_utente INT, categoria_selezionata VARCHAR(100), note_cliente VARCHAR(500), specifica_selezionata VARCHAR(500), quantità_specifica INT
  )
 BEGIN 
+
+SELECT tipo INTO @tipoutente FROM utente WHERE ID = id_utente;
+
+IF @tipoutente = 'utente' THEN 
+
 -- Metodo per trovare ID della categoria 
 SELECT ID INTO @id_categoria FROM categoria WHERE nome = categoria_selezionata;
 
 SELECT ID INTO @id_specifica FROM specifica WHERE nome = specifica_selezionata;
 
 -- Metodo per inserire la rischiera con i valori
-INSERT INTO richiesta_acquisto (ID_utente, `data`)
-VALUES (id_utente, NOW());
+INSERT INTO richiesta_acquisto (ID_utente, `data`,note)
+VALUES (id_utente, NOW(),note_cliente);
 
 -- Metodo per trovare ID della richista d'acquisto
 SET @ID_richiesta = LAST_INSERT_ID();
-
---
-IF note_cliente IS NULL AND note_cliente = '' THEN
-UPDATE richiesta_acquisto SET note = NULL WHERE ID = @ID_richiesta;
-ELSE
-UPDATE richiesta_acquisto SET note = note_cliente WHERE ID = @ID_richiesta;
-END IF;
 
 -- Metodo per associare l' ID della richiesta all' ID della categoria
 INSERT INTO seleziona (ID_richiesta_acquisto, ID_categoria)
@@ -29,13 +27,15 @@ VALUES (@ID_richiesta, @id_categoria);
 INSERT INTO associa (ID_categoria, ID_specifica,ID_richiesta_acquisto,quantità)
 VALUES (@id_categoria, @id_specifica,@ID_richiesta,quantità_specifica);
 
+    END IF;
+
 
  END //
         DELIMITER ;
         
-CALL CreareRichiesta( 3 , 'PERSONAL COMPUTER','','RAM',16);
-CALL CreareRichiesta( 1 , 'PERSONAL COMPUTER','','MEMORIA',32);
-CALL CreareRichiesta( 2 , 'COMPUTER FISSI','no','HERTZ',60);
+CALL CreareRichiesta( 10 , 'PERSONAL COMPUTER','','RAM',16);
+CALL CreareRichiesta( 5 , 'PERSONAL COMPUTER','','MEMORIA',32);
+CALL CreareRichiesta( 6 , 'COMPUTER FISSI','no','HERTZ',60);
 SELECT * FROM richiesta_acquisto;
 SELECT * FROM categoria;
 SELECT * FROM seleziona;
